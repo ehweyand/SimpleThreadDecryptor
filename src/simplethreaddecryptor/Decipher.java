3e95c65fd37538de3e2feedea4880c5e7299a7e3
Thomas M Guarnieri <thomas.guarnieri@universo.univates.br>
2021-03-21 20:39:32 -0300

    12 core threads

---
+++ src/simplethreaddecryptor/Decipher.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplethreaddecryptor;
+
+import static simplethreaddecryptor.SimpleThreadDecryptor.END_CHAR_CODE;
+import static simplethreaddecryptor.SimpleThreadDecryptor.REGEXP_WORDS;
+
+/**
+ *
+ * @author thom-mg
+ */
+public class Decipher implements Runnable{
+    private int sc;
+    private int[] encrypted;
+    private int expectedCheckSum;
+    
+    public Decipher(int sc, int[] encrypted, int expectedCheckSum) {
+        this.sc = sc;
+        this.encrypted = encrypted;
+        this.expectedCheckSum = expectedCheckSum;
+    }
+    
+    @Override
+    public void run() {
+        for (int i = sc ; i < END_CHAR_CODE; i++) {
+            for (int j = sc; j < END_CHAR_CODE; j++) {
+                for (int k = sc; k < END_CHAR_CODE; k++) {
+                    for (int l = sc; l < END_CHAR_CODE; l++) {
+                        tryToDecryptMsg("" + (char) i + (char) j + (char) k + (char) l, encrypted, expectedCheckSum);
+                    }
+                }
+            }
+        }
+    }
+    
+    private static void tryToDecryptMsg(String key, int[] msg, int checkSum) {
+        int x = 0;
+        int originalSum = 0;
+
+        String temp = "";
+        for (int i = 0; i < msg.length; i++) {
+            temp += (char) (msg[i] ^ key.charAt(x));
+            x++;
+
+            originalSum += (char) temp.charAt(i);
+            // Maior que 3, pois a chave tem 4 caracteres
+            if (x > 3) {
+                x = 0;
+            }
+        }
+
+        if (originalSum == checkSum && checkStringChars(temp)) {
+            System.out.println(temp);
+            System.out.println(originalSum);
+            System.out.println("Chave utilizada: " + key);
+        }
+
+    }
+    
+    private static boolean checkStringChars(String s) {
+        return s.matches(REGEXP_WORDS);
+    }
+    
+}
--- src/simplethreaddecryptor/SimpleThreadDecryptor.java
+++ src/simplethreaddecryptor/SimpleThreadDecryptor.java
@@ -9,13 +9,14 @@ package simplethreaddecryptor;
  *
  * @author evand
  */
-public class SimpleThreadDecryptor {
+public class SimpleThreadDecryptor{
 
     // Constantes
     static final int START_CHAR_CODE = 42;
     static final int END_CHAR_CODE = 122;
     static final String REGEXP_WORDS = "[a-zA-Z ]+";
     static final String REGEXP_WORDS_NUMBERS = "[a-zA-Z 0-9]+";
+    
 
     //Mensagem criptografada: {55,65,21,80,21,0,67,92,25,19,17,84,3,65,7,84,86,5,12,67,86,5,6,17,20,0,17,67,31,6,2}
     //Checksum da mensagem original: 2789
@@ -25,60 +26,16 @@ public class SimpleThreadDecryptor {
         int[] encrypted = {55, 65, 21, 80, 21, 0, 67, 92, 25, 19, 17, 84, 3, 65, 7, 84, 86, 5, 12, 67, 86, 5, 6, 17, 20, 0, 17, 67, 31, 6, 2};
         int expectedCheckSum = 2789;
         int encryptedLength = encrypted.length;
+        Thread t[] = new Thread[12];
 
         //Gerar todas as chaves de 4 letras e números possíveis
         // 4 fors encadeados e baseados no código da tabela ascii
-        decryptFourWordsKey(encrypted, expectedCheckSum);
-    }
-
-    private static void tryToDecryptMsg(String key, int[] msg, int checkSum) {
-        String aux = convertToString(msg);
-        int x = 0;
-        int originalSum = 0;
-
-        String temp = "";
-        for (int i = 0; i < aux.length(); i++) {
-            temp += (char) (aux.charAt(i) ^ key.charAt(x));
-            x++;
-
-            originalSum += (char) temp.charAt(i);
-            // Maior que 3, pois a chave tem 4 caracteres
-            if (x > 3) {
-                x = 0;
-            }
-        }
-
-        if (originalSum == checkSum && checkStringChars(temp)) {
-            System.out.println(temp);
-            System.out.println(originalSum);
-            System.out.println("Chave utilizada: " + key);
-        }
-
-    }
-
-    public static String convertToString(int[] message) {
-        String s = "";
-        for (int i : message) {
-            s += Character.toString((char) i);
-        }
-
-        return s;
-    }
-    // Start from character 118 to reduce processing
-    private static void decryptFourWordsKey(int[] encrypted, int expectedCheckSum) {
-        for (int i = END_CHAR_CODE - 4 ; i < END_CHAR_CODE; i++) {
-            for (int j = START_CHAR_CODE; j < END_CHAR_CODE; j++) {
-                for (int k = START_CHAR_CODE; k < END_CHAR_CODE; k++) {
-                    for (int l = START_CHAR_CODE; l < END_CHAR_CODE; l++) {
-                        tryToDecryptMsg("" + (char) i + (char) j + (char) k + (char) l, encrypted, expectedCheckSum);
-                    }
-                }
-            }
+        int toSum = 42;
+        for (int i = 0; i < 12; i++) {
+            t[i] = new Thread(new Decipher(toSum, encrypted, expectedCheckSum));
+            t[i].start();
+            
+            toSum += 7;
         }
     }
-
-    private static boolean checkStringChars(String s) {
-        return s.matches(REGEXP_WORDS);
-    }
-
 }
